a416e13481665d7898e5f7594693e813b0ba710e
Mikolaj Lenart <mikolaj.lenart@netguru.pl>
2018-12-18 12:17:15 +0100

    dding missing files

---
+++ app/src/test/java/co/netguru/robolectrictest/RobolectricTest.kt
@@ -0,0 +1,26 @@
+package co.netguru.robolectrictest
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class RobolectricTest {
+
+    @get:Rule
+    val rule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun exampledTest() {
+        Espresso.onView(ViewMatchers.withId(R.id.button)).perform(ViewActions.click())
+
+        Espresso.onView(ViewMatchers.withId(R.id.textView))
+            .check(ViewAssertions.matches(ViewMatchers.withText("Awsome ! You have clicked")))
+    }
+}
